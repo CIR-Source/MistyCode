36838f85138db075a3454ceb6e42f94f2b1eba79
CIR-Source <79338386+CIR-Source@users.noreply.github.com>
2024-03-04 21:43:50 -0600

    Add files via upload

---
+++ MistyCode/QuestionsData.js
@@ -0,0 +1,43 @@
+var threads = [
+    {
+        id: 1,
+        title: "Pregunta 1",
+        author: "Sigurd",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Crispy",
+                date: Date.now(),
+                content: "Texto de Respuesta"
+            },
+
+            {
+                author: "Omelet",
+                date: Date.now(),
+                content: "Texto de Respuesta"
+            }
+        ]
+    },
+
+    {
+        id: 2,
+        title: "Pregunta 2",
+        author: "Ray",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Crispy",
+                date: Date.now(),
+                content: "Texto de Respuesta"
+            },
+
+            {
+                author: "Omelet",
+                date: Date.now(),
+                content: "Texto de Respuesta"
+            }
+        ]
+    }
+]
